5e3fec1ed3708d7c2a4295de8beb8d22e30263ec
KorawichInt <korawich.inthamusik@gmail.com>
2024-07-14 12:14:27 +0700

    import os (preparing) and create function to read json from url

--- rongrean/rongrean/app.py
+++ rongrean/rongrean/app.py
@@ -1,7 +1,21 @@
 from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
+import os
+
+
+def read_and_save_json(url, save_path):
+    # if os.path.exists(save_path):
+    #     print(f"File already exists at {save_path}")
+    #     return
+    
+    df = pd.read_json(url)
+    print(df)
+    # df.to_json(save_path)
+    pass
 
 
 if __name__ == "__main__":
-    pass
+    url = "https://gpa.obec.go.th/reportdata/pp3-4_2566_province.json"
+    data_path = "student_data"
+    student_data = read_and_save_json(url, data_path)
