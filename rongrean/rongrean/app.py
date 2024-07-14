2e3a6268740f5c551d7421947f53a389817593ce
KorawichInt <korawich.inthamusik@gmail.com>
2024-07-14 13:22:31 +0700

    modify read_and_save_json by add return value (dataframe of student)

--- rongrean/rongrean/app.py
+++ rongrean/rongrean/app.py
@@ -8,16 +8,19 @@ def read_and_save_json(url, save_path):
     # check if file already exists
     if os.path.exists(save_path):
         print(f"File already exists at {save_path}")
-        return
+        df = pd.read_json(save_path)
+        return df
     
     # convert json to dataframe and save json file
     df = pd.read_json(url)
     df.to_json(save_path)
     print(f"Readed and saved to {save_path}")
+    return df
     
 
 
 if __name__ == "__main__":
     url = "https://gpa.obec.go.th/reportdata/pp3-4_2566_province.json"
     data_path = "student_data"
-    student_data = read_and_save_json(url, data_path)
+    student_df = read_and_save_json(url, data_path)
+    print(student_df)
