87ecd426e66d38ae883f47657bbc671c082fd18f
KorawichInt <korawich.inthamusik@gmail.com>
2024-07-14 21:20:19 +0700

    add checkbox of all, male, female to app layout

--- rongrean/rongrean/app.py
+++ rongrean/rongrean/app.py
@@ -5,10 +5,11 @@ import os
 
 app = Dash(__name__)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+# colors = {
+#     "background": "#111111",
+#     "ligh_blue_text": "#7FDBFF",
+#     "black_text": "#0D0000"
+# }
 
 def read_and_save_json(url, save_path):
     # check if file already exists
@@ -28,29 +29,35 @@ data_path = "student_data"
 student_df = read_and_save_json(url, data_path)
 
 filtered_df = student_df.loc[:, ["schools_province", "totalmale", "totalfemale", "totalstd"]]
-# app.layout = html.Div(style={"backgroundColor": colors["background"]}, 
-app.layout = html.Div(style={"backgroundColor": colors["background"]},
-    children = [
-        # Header (Content)
-        html.H1(
-            # children = "Mor.6 student (Graduated year 2566)"),
-            children = "จำนวนนักเรียนมัธยมศึกษาปีที่ 6 ที่จบการศึกษา ปีการศึกษา 2566",
-            style = {
-                "textAlign": "center",
-                "color": colors["text"]
-            }
-        ),
+# app.layout = html.Div(style={"backgroundColor": colors["background"], "color": colors["ligh_blue_text"]},
+app.layout = html.Div([
+    # Header (Content)
+    html.H2("จำนวนนักเรียนมัธยมศึกษาปีที่ 6 ที่จบการศึกษา ปีการศึกษา 2566"),
 
-        # Province dropdown
-        html.Label(
-            "จังหวัด", style = {"color": colors["text"]}
-        ),
-        dcc.Dropdown(filtered_df["schools_province"]),
+    # Dropdown for selecting province
+    html.Label("จังหวัดที่ต้องการแสดง"),
+    dcc.Dropdown(filtered_df["schools_province"].unique(), "กรุงเทพมหานคร"),
 
+    # Checkbox for selecting genders to display
+    html.Label("ประเภทที่ต้องการแสดง"),
+    dcc.Checklist(["ทั้งหมด", "นาย", "นางสาว"], ["ทั้งหมด"]),
 
-    ],
-)
-# ])
+    dcc.Graph(id="bar-graph")
+])
+
+def update_graph(selected_province, selected_data):
+    # Filter data based on selected province
+    filtered_province_df = filtered_df[filtered_df["schools_province"] == selected_province]
+    
+    # Create a bar graph
+    fig = px.bar(
+        filtered_province_df,
+        x="schools_province",
+        y=selected_data,
+        barmode="group"
+    )
+    
+    return fig
 
 
 
