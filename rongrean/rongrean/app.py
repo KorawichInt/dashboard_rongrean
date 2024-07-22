a8971ad263499aad1239d8d0a293e8cc465d40f5
KorawichInt <korawich.inthamusik@gmail.com>
2024-07-22 12:52:10 +0700

    modify app.py -> clean code, change cols name,  mod graph function

--- rongrean/rongrean/app.py
+++ rongrean/rongrean/app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output, callback, dash_table
 import plotly.express as px
 import pandas as pd
 import os
@@ -24,42 +24,64 @@ def read_and_save_json(url, save_path):
     print(f"Readed and saved to {save_path}")
     return df
 
+# Read json from url and save as a new json file
 url = "https://gpa.obec.go.th/reportdata/pp3-4_2566_province.json"
 data_path = "student_data"
 student_df = read_and_save_json(url, data_path)
 
 filtered_df = student_df.loc[:, ["schools_province", "totalmale", "totalfemale", "totalstd"]]
+filtered_df.columns = ["จังหวัด", "จำนวนนักเรียนชาย", "จำนวนนักเรียนหญิง", "จำนวนนักเรียนทั้งหมด"]
 # app.layout = html.Div(style={"backgroundColor": colors["background"], "color": colors["ligh_blue_text"]},
 app.layout = html.Div([
     # Header (Content)
     html.H2("จำนวนนักเรียนมัธยมศึกษาปีที่ 6 ที่จบการศึกษา ปีการศึกษา 2566"),
+    dash_table.DataTable(data=filtered_df.to_dict('records'), page_size=5),
 
     # Dropdown for selecting province
     html.Label("จังหวัดที่ต้องการแสดง"),
-    dcc.Dropdown(filtered_df["schools_province"].unique(), "กรุงเทพมหานคร"),
+    dcc.Dropdown(
+        id='dropdown-selection',
+        options=[{'label': province, 'value': province} for province in filtered_df['จังหวัด'].unique()],
+        # filtered_df["จังหวัด"].unique(), "กรุงเทพมหานคร"),
+        value='กรุงเทพมหานคร' # default
+    ),
 
     # Checkbox for selecting genders to display
-    html.Label("ประเภทที่ต้องการแสดง"),
-    dcc.Checklist(["ทั้งหมด", "นาย", "นางสาว"], ["ทั้งหมด"]),
+    html.Label("ตัวเลือกที่ต้องการแสดง"),
+    dcc.Checklist(
+        id='checklist-selection',
+        options=[
+            {'label': 'นักเรียนทั้งหมด', 'value': 'นักเรียนทั้งหมด'},
+            {'label': 'นักเรียนชาย', 'value': 'นักเรียนชาย'},
+            {'label': 'นักเรียนหญิง', 'value': 'นักเรียนหญิง'}
+        ],
+        value=['นักเรียนทั้งหมด'] # default
+    ),
 
-    dcc.Graph(id="bar-graph")
+    # Graph
+    dcc.Graph(id="graph-content")
 ])
 
-def update_graph(selected_province, selected_data):
+@callback(
+    Output('graph-content', 'figure'),
+    [Input('dropdown-selection', 'value'),
+     Input('checklist-selection', 'value')]
+)
+
+def update_graph(selected_province, selected_display):
     # Filter data based on selected province
-    filtered_province_df = filtered_df[filtered_df["schools_province"] == selected_province]
+    filtered_province_df = filtered_df[filtered_df["จังหวัด"] == selected_province]
     
     # Create a bar graph
     fig = px.bar(
         filtered_province_df,
-        x="schools_province",
-        y=selected_data,
-        barmode="group"
+        x='จังหวัด',
+        y=selected_display,
+        barmode='group'
+        
     )
     
     return fig
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
