061f7d42293f8d831235d3a8f8cf837ed74c094d
KorawichInt <korawich.inthamusik@gmail.com>
2024-07-22 18:05:20 +0700

    bold some topic font and clean webpage

--- rongrean/rongrean/app.py
+++ rongrean/rongrean/app.py
@@ -2,6 +2,9 @@ from dash import Dash, dcc, html, Input, Output, callback, dash_table
 import plotly.express as px
 import pandas as pd
 import os
+import plotly.graph_objects as go
+import json
+
 
 app = Dash(__name__)
 
@@ -23,6 +26,9 @@ student_df = read_and_save_json(url, data_path)
 filtered_df = student_df.loc[:, ["schools_province", "totalmale", "totalfemale", "totalstd"]]
 filtered_df.columns = ["จังหวัด", "นักเรียนชาย", "นักเรียนหญิง", "นักเรียนทั้งหมด"]
 
+with open('thailand_provinces.geojson', 'r', encoding='utf-8') as f:
+    geojson = json.load(f)
+
 app.layout = html.Div([
     html.H2(
         "จำนวนนักเรียนมัธยมศึกษาปีที่ 6 ที่จบการศึกษา ปีการศึกษา 2566", 
@@ -36,7 +42,16 @@ app.layout = html.Div([
 
     html.Div(
         [
-            html.Label("จังหวัดที่ต้องการแสดง"),
+            html.Label(
+                "จังหวัดที่ต้องการแสดง",
+                style={'font-weight':'bold'}
+            ),
+        ],
+        style={'margin-bottom': '10px'}
+    ),
+
+    html.Div(
+        [
             dcc.Dropdown(
                 id='dropdown-selection',
                 options=[{'label': province, 'value': province} for province in filtered_df['จังหวัด'].unique()],
@@ -48,7 +63,16 @@ app.layout = html.Div([
 
     html.Div(
         [
-            html.Label("ตัวเลือกที่ต้องการแสดง"),
+            html.Label(
+                "ตัวเลือกที่ต้องการแสดง",
+                style={'font-weight':'bold'}
+            ),
+        ],
+        style={'margin-bottom': '10px'}
+    ),
+
+    html.Div(
+        [
             dcc.Checklist(
                 id='checklist-selection',
                 options=[
@@ -60,10 +84,11 @@ app.layout = html.Div([
                 # inline=True
             ),
         ],
-        style={'margin-bottom': '10px'}
+        style={'margin-bottom': '20px'}
     ),
 
-    dcc.Graph(id="graph-content", style={'width': '100%'})
+    dcc.Graph(id="graph-content", style={'width': '100%'}),
+    dcc.Graph(id="map-content", style={'width': '100%'})
 ])
 
 @callback(
@@ -95,13 +120,51 @@ def update_graph(selected_province, selected_display):
             'xanchor': 'center',
             'yanchor': 'top'
         },
-        showlegend=False
         # width=800,
         # bargap=0.8
     )
 
     fig.update_traces(
         width=0.2
+    )
+    
+    return fig
+
+@callback(
+    Output('map-content', 'figure'),
+    [Input('dropdown-selection', 'value')]
+)
+
+def update_map(selected_province):
+    fig = px.choropleth(
+        filtered_df,
+        geojson=geojson,
+        locations='จังหวัด',
+        featureidkey="properties.name",
+        color='นักเรียนทั้งหมด',
+        color_continuous_scale="YlOrRd",
+        range_color=[filtered_df['นักเรียนทั้งหมด'].min(), filtered_df['นักเรียนทั้งหมด'].max()],
+        labels={'นักเรียนทั้งหมด': 'นักเรียนทั้งหมด'},
+        # width=2000,
+    )
+    
+    fig.update_geos(fitbounds="locations", visible=True)
+    
+    fig.update_layout(
+        title={
+            'text': "จำนวนนักเรียนทั้งหมดที่เรียนจบในแต่ละจังหวัด",
+            'font': {'size':20, 'weight':'normal'},
+            'pad':{'t':-20},
+            'y': 0.9,
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        coloraxis_colorbar={
+            'title': 'จำนวนนักเรียนทั้งหมด (คน)',
+            'x': 1.05,
+            'len': 0.8
+        }
     )
     
     return fig
